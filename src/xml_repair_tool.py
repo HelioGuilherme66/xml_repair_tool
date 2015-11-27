07a49a34d2e6b8d697de77fb795440d35ee5487f
Helio Guilherme <helioxentric@gmail.com>
2015-11-27 17:54:25 +0000

    First draft of the tool. Use in Windows like: python xml_repair_tool.py broken_xml.xml > fix.xml copy broken_xml.xml + fix.xml fixed_xml.xml (edit fixed_xml.xml to remove the last invalid character)

---
+++ src/xml_repair_tool.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+# This code was taken from:
+# http://stackoverflow.com/questions/10604048/pythonic-way-to-fix-broken-xml
+
+import sys
+from xml.sax import handler, make_parser
+
+class TagHandler(handler.ContentHandler):
+    def __init__(self):
+        handler.ContentHandler.__init__(self)
+
+        self.stack = []
+
+
+    def startElement(self, name, attrs):
+        self.stack.append(name)
+
+    def endElement(self, name):
+        # TODO: might want to just confirm that the element matches the top of the stack here
+        self.stack.pop()
+
+
+    def finish_document(self):
+        return "\n".join(["</%s>" % tag for tag in reversed(self.stack)])
+
+
+parser = make_parser()
+handler = TagHandler()
+parser.setContentHandler(handler)
+
+try:
+    parser.parse(sys.argv[1])
+
+except:
+    # TODO: something more intelligent than just printing out the
+    # constructed end of the document. Like appending it to the source
+    # and repeating whatever you did to make this processing necessary.
+    print handler.finish_document()
